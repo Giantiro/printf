f243d6a1aae477c77fa4d05c4bcd5714f773148d
Anthonia <giantonia001@gmail.com>
2022-10-19 14:23:16 -0700

    Header file adjusted

--- main.h
+++ main.h
@@ -51,7 +51,7 @@ char *convert(unsigned long int num, int base, int lowercase);
 int _printf(const char *format, ...);
 
 /* get_print */
-int (*get_print(char s))(va_list, flags_t *f);
+int (*get_print(char s))(va_list, flags_t *);
 
 /* get_flag */
 int get_flag(char s, flags_t *f);
